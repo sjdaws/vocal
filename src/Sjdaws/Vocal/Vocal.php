41e09407e3d20ae7053f07e65376742d7334c6a9
Scott Dawson <scott@uu.net.nz>
2015-02-12 12:10:45 +1100

    Add code explanation

--- src/Sjdaws/Vocal/Vocal.php
+++ src/Sjdaws/Vocal/Vocal.php
@@ -3,4 +3,12 @@
 namespace Sjdaws\Vocal;
 
 class Vocal extends Validation
-{}
+{
+    /**
+     * Hi! Looking for the code?
+     *
+     * - Code about dealing with rules, such as parsing and adding missing parameters is in Ruleset.php
+     * - Code for validation, such as actually running the validation is in Validation.php
+     * - Everything else is in SuperModel.php
+     */
+}
